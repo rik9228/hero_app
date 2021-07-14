60d6b07205179b5ba6d954a09e169044da37b7f8
RIKU <rank228130@gmail.com>
2021-07-14 22:57:42 +0900

    feature : CharacterList

---
+++ src/views/CharacterList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>キャラクタ一覧ページ</p>
+
+    <heroTable :headers="headers" :heroes="heroes" />
+  </div>
+</template>
+
+<script>
+import allHeroes from "@/assets/all.json";
+import HeroTable from "@/components/HeroTable.vue";
+
+export default {
+  name: "CharacterList",
+  components: {
+    HeroTable
+  },
+  data() {
+    return {
+      allHeroes: allHeroes
+    };
+  },
+  computed: {
+    headers() {
+      return [
+        { text: "Name", value: "name" },
+        { text: "PowerStatus", value: "power" },
+        { text: "publisher", value: "publisher" },
+        { text: "image", value: "image" },
+      ];
+    },
+    heroes() {
+      return this.allHeroes.map(hero => {
+        return {
+          name: hero.name,
+          power: hero.powerstats.intelligence,
+          publisher: hero.biography.publisher,
+          image: hero.images.xs,
+        };
+      });
+    }
+  }
+};
+</script>
+
+<style></style>
