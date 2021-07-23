86ea4efd4bfbc0790dd7b7fb4568949ec8725a21
RIKU <rank228130@gmail.com>
2021-07-23 23:48:34 +0900

    feature: dialog

--- src/views/CharacterList.vue
+++ src/views/CharacterList.vue
@@ -2,44 +2,108 @@
   <div>
     <p>キャラクタ一覧ページ</p>
 
-    <heroTable :headers="headers" :heroes="heroes" />
+    <heroTable :headers="headers" :heroes="heroes" @rowClick="onRowClick" />
+
+    <v-dialog v-model="showDetailModal" persistent>
+      <v-card>
+        <hero :hero="selectedHero" :showDetail="true" />
+        <!-- <h1>{{ selectedHero.name }}</h1> -->
+        <v-btn @click="closeDetailModal">閉じる</v-btn>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
 import allHeroes from "@/assets/all.json";
+import Hero from "@/components/Hero.vue";
 import HeroTable from "@/components/HeroTable.vue";
 
 export default {
   name: "CharacterList",
   components: {
-    HeroTable
+    HeroTable,
+    Hero,
   },
   data() {
     return {
-      allHeroes: allHeroes
+      allHeroes: allHeroes,
+      showDetailModal: false,
+      selectedHero: {},
     };
   },
   computed: {
     headers() {
       return [
-        { text: "Name", value: "name" },
+        { text: "image", value: "image" },
+        { text: "Name", value: "name", width: "20%" },
         { text: "PowerStatus", value: "power" },
         { text: "publisher", value: "publisher" },
-        { text: "image", value: "image" },
+        { text: "embrem", value: "embrem" },
       ];
     },
     heroes() {
-      return this.allHeroes.map(hero => {
+      return this.allHeroes.map((hero) => {
         return {
+          id: hero.id,
           name: hero.name,
           power: hero.powerstats.intelligence,
           publisher: hero.biography.publisher,
-          image: hero.images.xs,
+          image: hero.images.sm,
+          embrem: this.getEmbrem(hero.biography.publisher),
         };
       });
-    }
-  }
+    },
+    allPub() {
+      // 重複を消す
+      return Array.from(new Set(this.allHeroes.map((hero) => hero.biography.publisher)));
+    },
+    allPubCount() {
+      const x = this.allPub.map((pub) => {
+        return {
+          pubName: pub,
+          count: this.allHeroes.filter((hero) => hero.biography.publisher === pub).length,
+        };
+      });
+      return x.sort((a, b) => b.count - a.count);
+    },
+  },
+  methods: {
+    onRowClick(heroId) {
+      this.selectedHero = this.allHeroes.find((hero) => hero.id === heroId);
+      console.log(this.selectedHero);
+      this.showDetailModal = true;
+    },
+    closeDetailModal() {
+      this.showDetailModal = false;
+    },
+    getEmbrem(publisherName) {
+      if (
+        publisherName === "Marvel Comics" ||
+        publisherName === "Anti Venom" ||
+        publisherName === "Angel" ||
+        publisherName === "Evil Deadpool" ||
+        publisherName === "Deadpool" ||
+        publisherName === "Anti-Vision" ||
+        publisherName === "Ant-Man" ||
+        publisherName === "Rune King Thor"
+      ) {
+        return "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/2880px-Marvel_Logo.svg.png";
+      }
+      if (
+        publisherName === "DC Comics" ||
+        publisherName === "Oracle" ||
+        publisherName === "Spoiler" ||
+        publisherName === "Nightwing"
+      ) {
+        return "https://upload.wikimedia.org/wikipedia/commons/3/3d/DC_Comics_logo.svg";
+      }
+      if (publisherName === "Dark Horse Comics") {
+        return "https://www.seekpng.com/png/full/40-404080_dark-horse-wide-dark-horse-comics-logo-png.png";
+      }
+      return;
+    },
+  },
 };
 </script>
 
