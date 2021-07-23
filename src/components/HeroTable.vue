be47b15da28c7049bded811efbe0208ffe7a52ab
RIKU <rank228130@gmail.com>
2021-07-23 23:49:51 +0900

    feature: onRowClick

--- src/components/HeroTable.vue
+++ src/components/HeroTable.vue
@@ -5,17 +5,16 @@
     :items-per-page="-1"
     :hide-default-footer="true"
     class="elevation-1"
+    @click:row="onRowClick"
   >
-    <template v-slot:image :item="item">
-      <!-- <v-chip :color="getColor(item.image)" dark>
-        {{ item.image }}
-      </v-chip> -->
-      <!-- <v-avatar>
-        <img :src="item.image" alt="" />
-      </v-avatar> -->
+    <template v-slot:item.image="{ item }">
       <v-avatar>
         <img :src="item.image" alt="John" />
       </v-avatar>
+    </template>
+    <template v-slot:item.embrem="{ item }">
+      <!-- <img :src="item.embrem" alt="" /> -->
+      <v-img max-height="50" max-width="50" :src="item.embrem"></v-img>
     </template>
   </v-data-table>
 </template>
@@ -28,103 +27,12 @@ export default {
     heroes: { type: Array, require: true }
   },
   data() {
-    return {
-      // headers: [
-      //   {
-      //     text: "Dessert (100g serving)",
-      //     align: "start",
-      //     sortable: false,
-      //     value: "name"
-      //   },
-      //   { text: "Calories", value: "calories" },
-      //   { text: "Fat (g)", value: "fat" },
-      //   { text: "Carbs (g)", value: "carbs" },
-      //   { text: "Protein (g)", value: "protein" },
-      //   { text: "Iron (%)", value: "iron" }
-      // ],
-      // desserts: [
-      //   {
-      //     name: "Frozen Yogurt",
-      //     calories: 159,
-      //     fat: 6.0,
-      //     carbs: 24,
-      //     protein: 4.0,
-      //     iron: "1%"
-      //   },
-      //   {
-      //     name: "Ice cream sandwich",
-      //     calories: 237,
-      //     fat: 9.0,
-      //     carbs: 37,
-      //     protein: 4.3,
-      //     iron: "1%"
-      //   },
-      //   {
-      //     name: "Eclair",
-      //     calories: 262,
-      //     fat: 16.0,
-      //     carbs: 23,
-      //     protein: 6.0,
-      //     iron: "7%"
-      //   },
-      //   {
-      //     name: "Cupcake",
-      //     calories: 305,
-      //     fat: 3.7,
-      //     carbs: 67,
-      //     protein: 4.3,
-      //     iron: "8%"
-      //   },
-      //   {
-      //     name: "Gingerbread",
-      //     calories: 356,
-      //     fat: 16.0,
-      //     carbs: 49,
-      //     protein: 3.9,
-      //     iron: "16%"
-      //   },
-      //   {
-      //     name: "Jelly bean",
-      //     calories: 375,
-      //     fat: 0.0,
-      //     carbs: 94,
-      //     protein: 0.0,
-      //     iron: "0%"
-      //   },
-      //   {
-      //     name: "Lollipop",
-      //     calories: 392,
-      //     fat: 0.2,
-      //     carbs: 98,
-      //     protein: 0,
-      //     iron: "2%"
-      //   },
-      //   {
-      //     name: "Honeycomb",
-      //     calories: 408,
-      //     fat: 3.2,
-      //     carbs: 87,
-      //     protein: 6.5,
-      //     iron: "45%"
-      //   },
-      //   {
-      //     name: "Donut",
-      //     calories: 452,
-      //     fat: 25.0,
-      //     carbs: 51,
-      //     protein: 4.9,
-      //     iron: "22%"
-      //   },
-      //   {
-      //     name: "KitKat",
-      //     calories: 518,
-      //     fat: 26.0,
-      //     carbs: 65,
-      //     protein: 7,
-      //     iron: "6%"
-      //   }
-      // ]
-    };
+    return {};
+  },
+  methods: {
+    onRowClick(item) {
+      this.$emit("rowClick", item.id);
+    }
   }
 };
 </script>
