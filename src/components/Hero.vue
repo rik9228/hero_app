76912a8c61b86bd14ebf54d2a2014bb4d253e377
RIKU <rank228130@gmail.com>
2021-07-24 17:26:53 +0900

    update: HorizontalBarChart

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,26 +1,25 @@
 <!-- ヒーローデータの雛形内容を記入。具体的に入ってくるヒーロー（例えばバットマンとか）はここでは関係ない。親のコンポーネントからpropsで流し込まれる。 -->
 <template>
   <div :class="{ 'is-winner': isWinner, 'is-loser': isLoser }">
-    <v-card class="mx-auto pa-5" :class="{ selected }">
+    <v-card class="mx-auto pa-0" :class="{ selected }">
       <v-progress-circular v-if="!imgLoaded" indeterminate color="primary"></v-progress-circular>
       <!-- //オリジナル画像 -->
       <v-img
         v-if="!loadError"
         @error="errorImage"
         @load="onLoad"
-        class="image"
+        class="image mx-auto"
         :src="hero.image.url"
-        alt=""
       ></v-img>
       <!-- //デフォルト画像 -->
       <v-img
         v-if="loadError"
         @load="onLoad"
-        class="image"
+        class="image mx-auto"
         src="@/assets/img_noImage@2x.png"
       ></v-img>
 
-      <p class="text-center mt-5 mb-0 font-weight-medium" v-if="showName">
+      <p class="text-center ma-1 font-weight-medium" v-if="showName">
         {{ hero.name }}
       </p>
       <div class="powerstats" v-if="showDetail">
@@ -28,18 +27,18 @@
           <p class="text-center mt-2">出版社：{{ hero.biography.publisher }}</p>
           <p class="text-center mt-2">初登場：{{ hero.biography.firstAppearance }}</p>
           <ul class="powerstats__listFrame pl-0">
-            <li class="text-center mt-1 mb-1" v-if="showName">
+            <li class="text-center mt-2 mb-2" v-if="showName">
               知能：{{ hero.powerstats.intelligence }}
             </li>
-            <li class="text-center mt-1 mb-1" v-if="showName">
+            <li class="text-center mt-2 mb-2" v-if="showName">
               強さ：{{ hero.powerstats.strength }}
             </li>
-            <li class="text-center mt-1 mb-1" v-if="showName">速さ：{{ hero.powerstats.speed }}</li>
-            <li class="text-center mt-1 mb-1" v-if="showName">
+            <li class="text-center mt-2 mb-2" v-if="showName">速さ：{{ hero.powerstats.speed }}</li>
+            <li class="text-center mt-2 mb-2" v-if="showName">
               耐久性：{{ hero.powerstats.durability }}
             </li>
-            <li class="text-center mt-1 mb-1" v-if="showName">力：{{ hero.powerstats.power }}</li>
-            <li class="text-center mt-1 mb-1" v-if="showName">
+            <li class="text-center mt-2 mb-2" v-if="showName">力：{{ hero.powerstats.power }}</li>
+            <li class="text-center mt-2 mb-2" v-if="showName">
               戦闘力：{{ hero.powerstats.combat }}
             </li>
           </ul>
@@ -48,10 +47,10 @@
 
       <div class="d-flex">
         <div v-if="selectable">
-          <v-btn v-if="selected" depressed color="primary" @click="onSelect" class="mb-5">
+          <v-btn v-if="selected" depressed color="primary" @click="onSelect" class="ma-1">
             解除
           </v-btn>
-          <v-btn v-if="!selected" depressed color="primary" @click="onSelect" class="mb-5">
+          <v-btn v-if="!selected" depressed color="primary" @click="onSelect" class="ma-1">
             選択
           </v-btn>
         </div>
@@ -62,7 +61,17 @@
       <!-- <router-link :to="`/character/${id}`">詳細</router-link> -->
     </v-card>
     <!-- <button @click="addd">addd</button> -->
-    <HorizontalBarChart v-if="showBarChart" :chartData="chartData" :options="options" />
+
+    <div v-if="showBarChart">
+      <HorizontalBarChart
+        :chartData="chartData"
+        :options="options"
+        class="mt-5"
+        :height="200"
+        :width="200"
+      />
+      <p class="text-center text-h6 primary--text">ステータス合計値：{{ hero.totalStats }}</p>
+    </div>
   </div>
 </template>
 
@@ -76,9 +85,6 @@ export default {
   },
   props: {
     hero: { type: Object },
-    // id: { type: String },
-    // name: { type: String },
-    // imageUrl: { type: String },
     selectable: { type: Boolean, default: false },
     bookmarkable: { type: Boolean, default: false },
     selected: { type: Boolean, default: false },
@@ -119,19 +125,17 @@ export default {
   computed: {
     chartData() {
       const datacollection = {
-        labels: ["stats1"],
         datasets: [
           {
-            label: "Data1",
+            label: "ステータス合計値",
             backgroundColor: "blue",
-            // data: [Number(this.hero.powerstats.power)]
             data: [this.hero.totalStats]
           }
         ]
       };
       if (this.powerUp !== 0) {
         datacollection.datasets.push({
-          label: "Data2",
+          label: "勝利後に加わるステータス",
           backgroundColor: "red",
           data: [this.hero.powerUp]
         });
@@ -146,71 +150,15 @@ export default {
     }
   },
   methods: {
-    addd() {
-      this.datacollection = {
-        labels: ["January"],
-        datasets: [
-          {
-            label: "Data One",
-            backgroundColor: "#f87979",
-            data: [80]
-          }
-        ]
-      };
-    },
     onSelect() {
       this.$emit("select", this.hero);
     },
     onLoad() {
-      console.log("ロードされました");
       this.imgLoaded = true;
     },
     errorImage() {
-      console.log("エラーイメージ");
       this.loadError = true;
     }
   }
 };
 </script>
-
-<style>
-* {
-  list-style: none;
-}
-
-.selected {
-  background: red;
-}
-
-.d-flex {
-  display: flex;
-  justify-content: center;
-}
-
-.wrapper {
-  max-width: 960px;
-  width: calc(100% - 16px * 2);
-  margin: 0 auto;
-}
-
-.listFrame {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  column-gap: 40px;
-}
-
-.image {
-  max-width: 150px;
-  width: 100%;
-  margin: 0 auto;
-}
-
-.is-winner {
-  background: yellow;
-}
-
-.is-loser {
-  background: purple;
-}
-</style>
-alt="" alt=""
