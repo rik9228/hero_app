5b70b304f97314c5cf76936307022daeb27dedcc
RIKU <rank228130@gmail.com>
2021-07-24 17:28:37 +0900

    delete: data, computed

--- src/components/HeroList.vue
+++ src/components/HeroList.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="listFrame wrapper">
+  <ul class="listFrame wrapper pl-0">
     <li class="card" v-for="hero in info" :key="hero.id">
       <span v-if="hero.winner">負け</span>
       <hero
@@ -21,30 +21,15 @@ export default {
   components: {
     Hero
   },
-  data() {
-    return {
-      // selectedHeros: []
-    };
-  },
   props: {
     info: { type: Array },
     selectMode : { type: String, default: 'hero' },
-    // maxSelectCount: { type: Number, default: 3 }
-    // selectMode : { type: String },
-  },
-  computed: {
-    // ...mapGetters({
-    //   isSelected: "hero/isSelected" //メソッド
-    // })
   },
   methods: {
     isSelected(hero) {
       // Booleanを返す
-      // return this.selectedHeros.includes(id);
-      console.log("calle is selected", hero);
-      console.log("isSelected result=>", this.$store.getters["hero/isSelected"]({ hero, type: "villain" }));
-      // const fnc =this.$store.getters["hero/isSelected"];
-      // fnc({ hero, type: "villain" });
+      // console.log("calle is selected", hero);
+      // console.log("isSelected result=>", this.$store.getters["hero/isSelected"]({ hero, type: "villain" }));
       return this.$store.getters["hero/isSelected"]({ hero, type: this.selectMode });
       // ↑↑↑↑↑↑ ピュアなAPIを使っているのでcharacterTypeをつける必要がある。↑↑↑↑↑↑
       // （元のAPIにはcharacterTypeなんてものは存在しないから。コピーしたものには入っている）
