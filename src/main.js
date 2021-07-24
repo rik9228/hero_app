b103013b8ea8055a287f126f7add4b4e908ac71c
RIKU <rank228130@gmail.com>
2021-07-24 11:58:45 +0900

    feature: style.css

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify';
-import VueChartJs from 'vue-chartjs';
-
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import VueChartJs from "vue-chartjs";
+import css from "@/assets/css/style.css";
 
 Vue.use(VueChartJs);
 Vue.config.productionTip = false;
 
 new Vue({
+  css,
   router,
   store,
   vuetify,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
